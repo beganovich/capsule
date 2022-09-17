e260f3f993aeb4c3f01679358b2aca95064c983f
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-17 21:26:18 +0200

    Providing primitive types

--- tests/container.test.ts
+++ tests/container.test.ts
@@ -28,3 +28,18 @@ Deno.test("providing arguments manually", () => {
   assertEquals(userService.repoistory.get()[0].username, "foo");
   assertEquals(userService.repoistory.get()[1].username, "bar");
 });
+
+Deno.test("providing primitive types", () => {
+  class UserService {
+    constructor(public id: string) {}
+  }
+
+  container.set(UserService, UserService, () => '@id', {
+    position: 0,
+    property: "constructor",
+  });
+
+  const userService = container.get<UserService>(UserService);
+
+  assertEquals(userService.id, '@id');
+});
