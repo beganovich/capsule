e8f547dc2f7ef0351de64df2d80fd29ab2cde888
Benjamin Beganović <k1pstabug@gmail.com>
2022-09-17 21:22:28 +0200

    Providing arguments manually

--- src/container.ts
+++ src/container.ts
@@ -1,1 +1,77 @@
-export class Container {}
+type ProviderIdentifier = Symbol | Function;
+
+interface Provider {
+  identifier: ProviderIdentifier;
+  target: Function;
+  property: string | Symbol;
+  position: number;
+  callback: (container: Container) => unknown;
+}
+
+interface ProviderOptions {
+  property: string | Symbol;
+  position: number;
+}
+
+export class Container {
+  #arguments: Provider[] = [];
+
+  set(
+    identifier: ProviderIdentifier,
+    target: Function,
+    callback: (container: Container) => unknown,
+    options: ProviderOptions,
+  ): Container {
+    if (options.position < 0) {
+      throw new Error(
+        `Argument index can only be positive number. Trying to set [${options.position}].`,
+      );
+    }
+
+    this.#arguments.push({ identifier, target, callback, ...options });
+
+    return this;
+  }
+
+  get<T = unknown>(identifier: ProviderIdentifier, property = "constructor") {
+    const constructorArgs = this.#arguments.filter((provider) =>
+      provider.identifier === identifier || provider.target === identifier
+    );
+
+    const target = constructorArgs.length === 0
+      ? identifier
+      : constructorArgs[0].target;
+
+    const classArgs: unknown[] = [];
+
+    constructorArgs
+      .filter((arg) => arg.property === "constructor")
+      .sort((a, b) => a.position > b.position ? 1 : -1)
+      .filter((arg, index, self) =>
+        index === self.findIndex((prop) => (
+          prop.position === arg.position
+        ))
+      )
+      .map((arg) => classArgs.push(arg.callback(this)));
+
+    const object = Reflect.construct(target as Function, classArgs);
+
+    if (property === "constructor") {
+      return object as T;
+    }
+
+    const args: unknown[] = [];
+
+    constructorArgs
+      .filter((arg) => arg.property === property)
+      .sort((a, b) => a.position > b.position ? 1 : -1)
+      .filter((arg, index, self) =>
+        index === self.findIndex((prop) => (
+          prop.position === arg.position
+        ))
+      )
+      .map((arg) => args.push(arg.callback(this)));
+
+    return object[property](...args) as T;
+  }
+}
--- tests/container.test.ts
+++ tests/container.test.ts
@@ -3,6 +3,28 @@ import { assertEquals } from "../deps.ts";
 
 const container = new Container();
 
-Deno.test("example test", () => {
-  assertEquals(container instanceof Container, true);
+Deno.test("providing arguments manually", () => {
+  class UserRepository {
+    get() {
+      return [
+        { id: 1, username: "foo" },
+        { id: 2, username: "bar" },
+      ];
+    }
+  }
+
+  class UserService {
+    constructor(public repoistory: UserRepository) {}
+  }
+
+  container.set(UserService, UserService, () => new UserRepository(), {
+    position: 0,
+    property: "constructor",
+  });
+
+  const userService = container.get<UserService>(UserService);
+
+  assertEquals(userService.repoistory instanceof UserRepository, true);
+  assertEquals(userService.repoistory.get()[0].username, "foo");
+  assertEquals(userService.repoistory.get()[1].username, "bar");
 });
